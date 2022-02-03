552d81286b4231e46bc0fd0b87e533fb1487bf1b
Johan Euphrosine <proppy@google.com>
2022-02-03 22:57:18 +0900

    misc/open_pdks: embed skywater build

--- misc/open_pdks/build-variant.sh
+++ misc/open_pdks/build-variant.sh
@@ -1,8 +1,27 @@
 #!/bin/bash
 set -ex
 
+# make timing
+for LIB in skywater-pdk/libraries/sky130_*_sc_*/latest; do
+  if [ -d "$LIB/cells" ]; then
+    python -m skywater_pdk.liberty $LIB
+    python -m skywater_pdk.liberty $LIB all
+    python -m skywater_pdk.liberty $LIB all --ccsnoise
+  fi
+done
+
+# extract variant name from package name
 VARIANT=${PKG_NAME#open_pdks.sky130}
 VARIANT=${VARIANT^^}
-./configure --prefix=$PREFIX --enable-sky130-pdk=$PREFIX/share/skywater-pdk/ --disable-alpha-sky130 --disable-xschem-sky130 --with-sky130-variants=$VARIANT
+
+# --enable-sky130-pdk: point to current checkout
+# --disable-alpha-sky130: disable font library
+# --disable-xschem-sky130: disable xschem integration
+# --with-sky130-variants: use specified variant 
+./configure --prefix=$PREFIX \
+  --enable-sky130-pdk=$SRC_DIR/skywater-pdk/ \
+  --disable-alpha-sky130 \
+  --disable-xschem-sky130 \
+  --with-sky130-variants=$VARIANT
 make
 make install
