a634f132e4922ebd3666320259219c7c61c0908d
Johan Euphrosine <proppy@google.com>
2022-09-27 21:07:27 +0900

    misc/openlane: untie from pdk

--- misc/openlane/env.tcl
+++ misc/openlane/env.tcl
@@ -6,8 +6,5 @@ if { ! [info exists ::env(TCLLIBPATH)] } {
     set ::env(TCLLIBPATH) [glob -type d "/usr/share/tcltk/tcllib*"]
 }
 lappend ::auto_path $::env(TCLLIBPATH)
-# define PDK variables
+# default to conda-install PDKs
 set ::env(PDK_ROOT) "$::env(CONDA_PREFIX)/share/pdk"
-set ::env(PDK) "sky130A"
-set ::env(STD_CELL_LIBRARY) "sky130_fd_sc_hd"
-set ::env(STD_CELL_LIBRARY_OPT) "sky130_fd_sc_hd"
