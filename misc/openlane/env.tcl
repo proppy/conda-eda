f819e0f927e34766ec0334b85b5a9d29e1215c06
Johan Euphrosine <proppy@google.com>
2022-10-13 18:09:36 +0900

    misc/openlane: use packaged tcllib

--- misc/openlane/env.tcl
+++ misc/openlane/env.tcl
@@ -2,9 +2,7 @@
 set ::env(OPENLANE_ROOT) "$::env(CONDA_PREFIX)/share/openlane"
 set ::env(OL_INSTALL_DIR) "$::env(CONDA_PREFIX)/share/openlane/install"
 set ::env(OPENLANE_LOCAL_INSTALL) 1
-if { ! [info exists ::env(TCLLIBPATH)] } {
-    set ::env(TCLLIBPATH) [glob -type d "$::env(CONDA_PREFIX)/lib/tcllib*"]
-}
+set ::env(TCLLIBPATH) [glob -type d "$::env(CONDA_PREFIX)/lib/tcllib*"]
 lappend ::auto_path $::env(TCLLIBPATH)
 # default to conda-install PDKs
 set ::env(PDK_ROOT) "$::env(CONDA_PREFIX)/share/pdk"
