6e4df825e838e4f3251952f02c5f081cf8f47d1f
Johan Euphrosine <proppy@google.com>
2022-12-25 11:02:14 +0900

    sim/ngspice: enable more options

--- sim/ngspice/build.sh
+++ sim/ngspice/build.sh
@@ -13,6 +13,6 @@ case "${UNAME_OUT}" in
 esac
 
 ./autogen.sh
-./configure --prefix="${PREFIX}" --disable-debug
+./configure --prefix="${PREFIX}" --with-x --enable-xspice --enable-cider --with-readline=yes --enable-openmp --disable-debug
 make V=1 -j$CPU_COUNT
 make V=1 install
