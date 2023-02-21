bdcb477a07078d88461282c19575b1517e71cbc0
Johan Euphrosine <proppy@google.com>
2023-02-21 15:17:27 +0900

    installer: fix prefix

--- installer/analog.gf180mcuc/post_install.sh
+++ installer/analog.gf180mcuc/post_install.sh
@@ -14,11 +14,11 @@ cat > $PREFIX/etc/conda/activate.d/klayout_activate.sh <<EOF
 export KLAYOUT_PATH=\$CONDA_PREFIX/share/pdk/gf180mcuC/libs.tech/klayout
 EOF
 
-ln -s $CONDA_PREFIX/share/pdk/gf180mcuC/libs.tech/magic/gf180mcuC.magicrc $PREFIX/lib/magic/sys/site.def
+ln -s $PREFIX/share/pdk/gf180mcuC/libs.tech/magic/gf180mcuC.magicrc $PREFIX/lib/magic/sys/site.def
 
 cat >> $PREFIX/share/pdk/gf180mcuC/libs.tech/xschem/xschemrc <<EOF
 set XSCHEM_START_WINDOW \${PDK_ROOT}/gf180mcuC/libs.tech/xschem/tests/0_top.sch
 append XSCHEM_LIBRARY_PATH :\${PDK_ROOT}/gf180mcuC/libs.tech/xschem
 EOF
 sed -i -e 's/ngspice\/models/gf180mcuC\/libs.tech\/ngspice/' $PREFIX/share/pdk/gf180mcuC/libs.tech/xschem/xschemrc 
-ln -sf $CONDA_PREFIX/share/pdk/gf180mcuC/libs.tech/xschem/xschemrc $CONDA_PREFIX/share/xschem/xschemrc
+ln -sf $PREFIX/share/pdk/gf180mcuC/libs.tech/xschem/xschemrc $PREFIX/share/xschem/xschemrc
